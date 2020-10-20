419f154d1802122937d0a11aa67b4158cbdb576b
Olena Shchadylo <olenashchadylo@MacBook-Pro-Olena.local>
2020-10-20 18:18:44 +0300

    Connected MongoDB server and Creacted ProductRouter

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const Products = require('./models/product.model');
+
+const url = 'mongodb://localhost:27017/shopServer';
+const connect = mongoose.connect(url, { useNewUrlParser: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product.route');
+
+connect.then((db) => {
+  console.log("Connected correctly");
+}, (err) => { console.log(err); });
 
 var app = express();
 
@@ -21,14 +32,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
